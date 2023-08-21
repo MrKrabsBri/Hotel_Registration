a549135fe192ef7e86e1ff97305cd8215c13983f
Julius Bri <Julius Bri>
2023-08-21 10:14:28 +0300

    Room class createdHotel

---
+++ src/main/java/org/Brikas/Hotel/BrikasHotel.java
@@ -0,0 +1,28 @@
+package org.Brikas.Hotel;
+
+import java.util.LinkedList;
+
+public class HotelRoom {
+   // private int[] rooms = 5;
+   // LinkedList<String>roomss = new LinkedList<>();//nehardcodink
+
+   public void generateHotel(LinkedList<String>rooms){ // del sito gali nesaugoti
+       rooms.add("room nr.1");
+       rooms.add("room nr.2");
+       rooms.add("room nr.3");
+       rooms.add("room nr.4");
+       rooms.add("room nr.5");
+   }
+
+   public boolean isRoomEmpty(LinkedList<String>rooms){
+
+
+
+       return true;
+   }
+
+   public void registerToTheRoom(LinkedList<String>rooms){
+
+   }
+
+}
