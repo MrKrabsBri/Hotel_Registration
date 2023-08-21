509171d067a32ad6628ee1a020a981cc6d3881d2
Julius Bri <Julius Bri>
2023-08-21 08:17:03 +0300

    hotel registration start

---
+++ src/main/java/org/Brikas/Hotel/HotelReg.java
@@ -0,0 +1,10 @@
+package org.Brikas.Hotel;
+
+import java.util.LinkedList;
+
+public class HotelReg {
+   // private int[] rooms = 5;
+    private LinkedList<String>rooms = new LinkedList<>();
+
+
+}
--- src/main/java/org/Brikas/Hotel/HotelRegistration.java
+++
@@ -1,4 +0,0 @@
-package org.Brikas.Hotel;
-
-public class HotelRegistration {
-}
