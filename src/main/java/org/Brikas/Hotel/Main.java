50752d67ff10152655407f875e16feaae57d0dc2
MrKrabsBri <116517688+MrKrabsBri@users.noreply.github.com>
2025-11-05 17:40:04 +0200

    project cleanup

--- src/main/java/org/Brikas/Hotel/Main.java
+++ src/main/java/org/Brikas/Hotel/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
         final String filename = "HotelRoomsSave.bin";
-        //System.out.println("Working Directory: " + System.getProperty("user.dir"));
 
         Scanner scanner = new Scanner(System.in);
 
